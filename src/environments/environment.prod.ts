71f35e0a375f39d013d40f03ad4e2fade2241387
bdiniscia <bdiniscia@gmail.com>
2020-07-18 23:52:41 -0400

    Adding info to the Readme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC8jSr1WmoIf2cvYu2TcuKc9vUx3GBuOCc",
+    authDomain: "to-do-list-angular-ab3f6.firebaseapp.com",
+    databaseURL: "https://to-do-list-angular-ab3f6.firebaseio.com",
+    projectId: "to-do-list-angular-ab3f6",
+    storageBucket: "to-do-list-angular-ab3f6.appspot.com",
+    messagingSenderId: "9989405588",
+    appId: "1:9989405588:web:45ed9492d64bb70be52d81"
+  }
 };
